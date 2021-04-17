3a48b69c97a8aeded91f3c9871ffb9362411cc37
Swapnil Rana <50835521+swapy-S@users.noreply.github.com>
2021-04-17 19:28:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,13 @@ from io import StringIO
 
 app = Flask(__name__)
 
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+
 @app.route("/")
 def home():
     return render_template("home.html",name="swapnil")
